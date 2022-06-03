b158127a3a4f1fa679903c2d67cb69e897df9167
Julie Swoope <julie.swoope@withcoherence.com>
2022-06-03 13:54:13 +0000

    add test command

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -1,12 +1,14 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
+
+puts "\n\n\n HELLLOOOOOO \n\n\n"
+
 require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+x  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
